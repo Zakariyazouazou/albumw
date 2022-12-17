2693b3a5ef1d81cbf6586a336618b1fcec058ea5
Zakariyazouazou <112694786+Zakariyazouazou@users.noreply.github.com>
2022-12-17 11:39:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ const App = () => {
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#e36d12]">
         <Searchbar placeHolder='write your song.....' data={dataSong}/>
-
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
